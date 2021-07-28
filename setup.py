f2ffff3d2189bf5ec03c6edc5b9f95fe7f45e70a
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-07-28 11:35:28 +0200

    Rewrite setup.py to setup.cfg
    Updated the metadata of the package, as I have taken over as maintainer
    after kalombos.

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# 'make build' will build the pynetsnmp and copy them to the needed locations.
-
-setup(
-    name="pynetsnmp-2",
-    version="0.1.4",
-    packages=find_packages(),
-    install_requires=[
-        'setuptools',
-        'ipaddr',
-        'Twisted',
-        'six',
-    ],
-    include_package_data=True,
-    # metadata for upload to PyPI
-    author="Zenoss",
-    author_email="support@zenoss.com",
-    maintainer='kalombo',
-    maintainer_email='nogamemorebrain@gmail.com',
-    description="ctypes wrapper for net-snmp.",
-    long_description="This repo is a fork of https://github.com/zenoss/pynetsnmp "
-                     "with opportunity to use set method.",
-    license="GPLv2 or later",
-    keywords=['snmp', 'twisted', 'pynetsnmp', 'netsnmp'],
-    url="https://github.com/kalombos/pynetsnmp",
-    download_url="https://github.com/kalombos/pynetsnmp",
-    zip_safe=False
-)
+setup()
